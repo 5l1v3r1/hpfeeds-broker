125c8136e2f63fda78f4a113cdcb608a010198b8
Brady Sullivan <brady@bsull.com>
2020-02-20 20:10:29 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"github.com/d1str0/hpfeeds"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	var verbose bool
+	flag.BoolVar(&verbose, "verbose", false, "output lots of stuff")
+	flag.Parse()
+
+	c := CreateClient()
+	b := &hpfeeds.Broker{
+		Name: "mhn",
+		Port: 10000,
+		DB:   c,
+	}
+
+	if verbose {
+		b.SetDebugLogger(log.Print)
+		b.SetInfoLogger(log.Print)
+		b.SetErrorLogger(log.Print)
+	}
+
+	err := b.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type MongoClient struct {
+	*mongo.Client
+}
+
+func CreateClient() *MongoClient {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &MongoClient{client}
+}
+
+func (c *MongoClient) Identify(ident string) (*hpfeeds.Identity, error) {
+	// Hardcoded for MHN installs
+	collection := c.Database("hpfeeds").Collection("auth_key")
+
+	var result hpfeeds.Identity
+	filter := bson.M{"ident": ident}
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	err := collection.FindOne(ctx, filter).Decode(&result)
+	return &result, err
+}
